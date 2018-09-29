fa325150c68150913dbd74b972fde33b9a439af8
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2018-09-29 14:40:51 +0200

    addded CSS for categories

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import api from "../api.js";
 
+
+
 class ProductList extends React.Component {
     constructor(props) {
         super(props)
@@ -27,25 +29,42 @@ class ProductList extends React.Component {
     render() {
         const { productArray } = this.state;
         const productList = productArray.map((oneProduct)=> {
+            // const backgroundStyle = {
+            //     backgroundImage:`url(${oneProduct.acf.product_image.url})`,
+            // };
             return <li key={oneProduct.id}>
-                        <h2>{oneProduct.acf.product_title}</h2>
-                        <img src={oneProduct.acf.product_image.url} />
+                        <div className="product-list-item">
+                            <img src={oneProduct.acf.product_image.url} />
+                        </div>
+                        <div className="product-list-overlay">
+                            <h2>{oneProduct.acf.product_title}</h2>
+                        </div>
                     </li>
         });
         console.log("this is it", productArray);
 
         return (
-            <section className="product-list">
-                <ul>
-                    
-                </ul>
-                <ul>
-                    {productList}
-                </ul>
+            <section className="product-list-page">
+                <div className="head-banner">
+                <h1>Category</h1>
+                </div>
+                <div className="container">
+                    <ul className="filter-list">
+                        <li>Type</li>
+                        <li>Colour</li>
+                        <li>Shape</li>
+                        <li>Materials</li>
+                    </ul>
+                    <ul className="product-list">
+                        {productList}
+                    </ul>
+                </div>
             </section>
         );
 
     }
 }
+
+
 
 export default ProductList;
