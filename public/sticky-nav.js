42726b1f5e166f416e3cb266faedc75ad939654d
Pauline Brothier <paulinebrothier@gmail.com>
2018-10-02 12:37:54 +0200

    debbuging stuff

--- public/sticky-nav.js
+++ public/sticky-nav.js
@@ -6,20 +6,19 @@ window.onscroll = function () { myFunction() };
 function myFunction() {
     // Get the header
     var header = document.getElementById("header");
-    console.log("header:", header);
+    // console.log("header:", header);
 
     if (!header) {
         return;
     }
 
     // Get the offset position of the navbar
-    var sticky = header.offsetTop;
+    // var sticky = header.offsetTop;
 
-    console.log(window.pageYOffset);
-    if (window.pageYOffset >= sticky) {
+
+    if (window.pageYOffset > 1000) {
         header.classList.add("sticky")
     } else {
         header.classList.remove("sticky");
     }
 }
-
