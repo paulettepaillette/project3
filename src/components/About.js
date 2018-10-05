904fcebecbe4137d53bf9786538ee54039dd76d8
Mohamed ZAMAMA <sm.zamama@gmail.com>
2018-10-05 14:44:06 +0200

    parallax change

--- src/components/About.js
+++ src/components/About.js
@@ -54,8 +54,8 @@ class About extends Component {
                                         <div className="our-misssion">
                                             <Parallax
                                                 className="custom-class"
-                                                offsetXMax={10}
-                                                offsetXMin={-10}
+                                                // offsetXMax={10}
+                                                // offsetXMin={-10}
                                                 offsetYMax={10}
                                                 offsetYMin={-10}
                                                 slowerScrollRate
@@ -76,17 +76,9 @@ class About extends Component {
                                                 <h2>{oneData.acf.our_technology_title}</h2>
                                                 <div dangerouslySetInnerHTML={{ __html: oneData.acf.our_technology_text }} />
                                             </div>
-                                            <Parallax
-                                                className="custom-class"
-                                                offsetXMax={10}
-                                                offsetXMin={-10}
-                                                offsetYMax={10}
-                                                offsetYMin={-10}
-                                                slowerScrollRate
-                                                tag="figure"
-                                                > 
+                                            
                                         <img src={oneData.acf.our_technology_image.url} alt={oneData.acf.our_technology_image.alt} />
-                                        </Parallax>
+                                        
                                         </div>
 
                                         <div className="video">
